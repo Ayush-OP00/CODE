10ceaca5022809231af8a4992e1d722ca61bf000
Ayush-OP00 <allinone7925@gmail.com>
2026-02-20 15:54:15 +0530

    The commit adds the book history that accounts for the issue/return of books marking the availability.

--- LibraryManagement/main.py
+++ LibraryManagement/main.py
@@ -1,4 +1,5 @@
 #This program is dedicated to the creation of library management system where we have three classe :BOOK, BOOK HISTORY, USER 
+#This 3 classes will be governed and utilized by Admin class which will be responsible for the management of the library system. The Admin class will have the ability to add books, issue books to users, and manage user information. 
 class book:
     def __init__(self, id, title, author, units, available_units):
         self.id = id
@@ -6,3 +7,10 @@ class book:
         self.author = author
         self.units = units
         self.available_units = available_units
+class book_history:
+    def __init__(self, user, book, issue_date, return_date):
+        self.user = user
+        self.book = book
+        self.issue_date = issue_date
+        self.return_date = return_date
+
