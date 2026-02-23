f3de6468cc955b595eb1c7a712b1670bc7bb06dc
Ayush-OP00 <allinone7925@gmail.com>
2026-02-23 15:54:16 +0530

    new branch

--- LibraryManagement/main.py
+++ LibraryManagement/main.py
@@ -1,5 +1,8 @@
 #This program is dedicated to the creation of library management system where we have three classe :BOOK, BOOK HISTORY, USER 
 #This 3 classes will be governed and utilized by Admin class which will be responsible for the management of the library system. The Admin class will have the ability to add books, issue books to users, and manage user information. 
+from turtle import title
+
+
 class book:
     def __init__(self, id, title, author, units, available_units):
         self.id = id
@@ -28,11 +31,38 @@ class admin:
         self.books = []
         self.users = []
         self.book_history = []
+        self.issued_books = {}  
         def add_book(self, id, title, author, units):
             new_book = book(id, title, author, units, available_units=units)
             self.books.append(new_book)
         def add_user(self, id, name, roll_no, email, mobile, semester, department):
             new_user = user(id, name, roll_no, email, mobile, semester, department)
             self.users.append(new_user)
-        def issue_book(self, user_id, book_id, issue_date, return_date):
-            
+class admin:
+    def __init__(self):
+        self.books = []  # List of books
+        self.users = []  # List of users
+        self.book_history = []  # List of book transactions
+        self.issued_books = {}  # Dictionary to track issued books: {user_id: [book_ids]}
+    
+    def add_book(self, id, title, author, units):
+        """Add a new book to the library"""
+        new_book = book(id, title, author, units, available_units=units)
+        self.books.append(new_book)
+        print(f"Book '{title}' added successfully!")
+        return True
+    
+    def add_user(self, id, name, roll_no, email, mobile, semester, department):
+        """Add a new user to the library"""
+        new_user = user(id, name, roll_no, email, mobile, semester, department)
+        self.users.append(new_user)
+        print(f"User '{name}' added successfully!")
+        return True   
+    def display_books(self):
+        """Display all books in the library"""
+        if not self.books:
+            print("No books available in the library.")
+            return
+        print("Books in the library:")
+        for book in self.books:
+            print(f"ID: {book.id}, Title: {book.title}, Author: {book.author}, Units: {book.units}, Available Units: {book.available_units}")
---
+++ characterCount.py
@@ -0,0 +1,32 @@
+a= "python session"
+count = 0
+for i in a:
+    if i == "p":
+        count += 1
+        print("p= ", count)
+    elif i == "y":
+        count += 1
+        print("y= ", count)
+    elif i == "t":
+        count += 1
+        print("t= ", count)     
+    elif i == "h":
+        count += 1
+        print("h= ", count)
+    elif i == "o":
+        count += 1
+        print("o= ", count)
+    elif i == "n":
+        count += 1
+        print("n= ", count)
+    elif i == "s":
+        count += 1
+        print("s= ", count)
+    elif i == "e":
+        count += 1
+        print("e= ", count)
+    elif i == "i":
+        count += 1
+        print("i= ", count)
+else :
+    print("Invalid character")
