2fe8c61bd9517e3fdcb8efddba6e45329c92dc88
Ayush-OP00 <allinone7925@gmail.com>
2026-02-25 13:59:09 +0530

    main update

--- LibraryManagement/main.py
+++ LibraryManagement/main.py
@@ -1,22 +1,13 @@
-#This program is dedicated to the creation of library management system where we have three classe :BOOK, BOOK HISTORY, USER 
-#This 3 classes will be governed and utilized by Admin class which will be responsible for the management of the library system. The Admin class will have the ability to add books, issue books to users, and manage user information. 
-from turtle import title
-
+#This is a simple Library Management System (LMS) implemented in Python. It allows you to manage books and users in a library. The system uses classes to represent books and users, and dictionaries to store the records. The program takes input for books and users, and then displays the records. This is a basic implementation & cna be further enhanced with admin panel to add, delete, update records, issue books to users, and track book history.
 
 class book:
-    def __init__(self, id, title, author, units, available_units):
+    def __init__(self, id, title, author, units):
         self.id = id
         self.title = title
         self.author = author
         self.units = units
-        self.available_units = available_units
-class book_history:
-    def __init__(self, user, id, book, issue_date, return_date):
-        self.user = user
-        self.id = id
-        self.book = book
-        self.issue_date = issue_date
-        self.return_date = return_date
+        self.available_units = units
+
 class user:
     def __init__(self, id, name, roll_no, email, mobile, semester, department):
         self.id = id
@@ -26,43 +17,46 @@ class user:
         self.mobile = mobile
         self.semester = semester
         self.department = department
-class admin:
-    def __init__(self):
-        self.books = []
-        self.users = []
-        self.book_history = []
-        self.issued_books = {}  
-        def add_book(self, id, title, author, units):
-            new_book = book(id, title, author, units, available_units=units)
-            self.books.append(new_book)
-        def add_user(self, id, name, roll_no, email, mobile, semester, department):
-            new_user = user(id, name, roll_no, email, mobile, semester, department)
-            self.users.append(new_user)
-class admin:
-    def __init__(self):
-        self.books = []  # List of books
-        self.users = []  # List of users
-        self.book_history = []  # List of book transactions
-        self.issued_books = {}  # Dictionary to track issued books: {user_id: [book_ids]}
-    
-    def add_book(self, id, title, author, units):
-        """Add a new book to the library"""
-        new_book = book(id, title, author, units, available_units=units)
-        self.books.append(new_book)
-        print(f"Book '{title}' added successfully!")
-        return True
-    
-    def add_user(self, id, name, roll_no, email, mobile, semester, department):
-        """Add a new user to the library"""
-        new_user = user(id, name, roll_no, email, mobile, semester, department)
-        self.users.append(new_user)
-        print(f"User '{name}' added successfully!")
-        return True   
-    def display_books(self):
-        """Display all books in the library"""
-        if not self.books:
-            print("No books available in the library.")
-            return
-        print("Books in the library:")
-        for book in self.books:
-            print(f"ID: {book.id}, Title: {book.title}, Author: {book.author}, Units: {book.units}, Available Units: {book.available_units}")
+
+
+#DICTIONARIES 
+books = {}   # key = book_id, value = book object
+users = {}   # key = user_id, value = user object
+
+
+# TAKING BOOK INPUT 
+print("Enter details for 3 books:\n")
+
+for i in range(3):
+    print(f"\nBook {i+1}")
+    id = int(input("Enter Book ID: "))
+    title = input("Enter Title: ")
+    author = input("Enter Author: ")
+    units = int(input("Enter Units: "))
+
+    new_book = book(id, title, author, units)
+    books[id] = new_book   # storing in dictionary
+
+print("\nEnter details for 3 users:\n")
+
+for i in range(3):
+    print(f"\nUser {i+1}")
+    id = int(input("Enter User ID: "))
+    name = input("Enter Name: ")
+    roll = input("Enter Roll No: ")
+    email = input("Enter Email: ")
+    mobile = input("Enter Mobile: ")
+    sem = int(input("Enter Semester: "))
+    dept = input("Enter Department: ")
+
+    new_user = user(id, name, roll, email, mobile, sem, dept)
+    users[id] = new_user   # storing in dictionary
+
+
+print(" BOOK RECORDS ")
+for key, value in books.items():
+    print(f"ID: {key}, Title: {value.title}, Author: {value.author}, Available: {value.available_units}/{value.units}")
+
+print(" USER RECORDS ")
+for key, value in users.items():
+    print(f"ID: {key}, Name: {value.name}, Roll No: {value.roll_no}, Email: {value.email}")
